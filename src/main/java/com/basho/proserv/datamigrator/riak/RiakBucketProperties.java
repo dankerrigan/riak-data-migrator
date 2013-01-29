282e9141f616a977bf9a599d9b048fbff19ebd32
Dan Kerrigan <dankerrigan@basho.com>
2013-01-29 13:51:48 -0500

    Riak Bucket Properties serialization routines for perserving bucket settings

---
+++ src/main/java/com/basho/proserv/datamigrator/riak/RiakBucketProperties.java
@@ -0,0 +1,88 @@
+package com.basho.proserv.datamigrator.riak;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.basho.riak.client.bucket.BucketProperties;
+import com.thoughtworks.xstream.XStream;
+
+public class RiakBucketProperties {
+	private Logger log = LoggerFactory.getLogger(RiakBucketProperties.class);
+	private final Connection connection;
+	public RiakBucketProperties(Connection connection) {
+		this.connection = connection;
+	}
+	
+	public BucketProperties getBucketProperties(String bucketName) {
+		BucketProperties bucketProperties = null;
+		try {
+			bucketProperties = this.connection.riakClient.fetchBucket(bucketName);
+		} catch (IOException e) {
+			log.error("Coudldn't retrieve bucket");
+			return null;
+		}
+		return bucketProperties;
+	}
+	
+	public boolean setBucketProperties(String bucketName, BucketProperties bucketProperties) {
+		boolean success = true;
+		try {
+			this.connection.riakClient.updateBucket(bucketName, bucketProperties);
+		} catch (IOException e) {
+			log.error("Couldn't set bucket properties for bucket " +  bucketName);
+			success = false;
+		}
+		
+		return success;
+	}
+	
+	public boolean writeBucketProperties(BucketProperties props, File file) {
+		XStream xStream = new XStream();
+		FileWriter writer;
+		try {
+			writer = new FileWriter(file);
+		} catch (IOException e) {
+			log.error("Could not write bucket properties to file " + file.getAbsolutePath(), e);
+			return false;
+		}
+		xStream.toXML(props, writer);
+		try {
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			log.error("Could not close bucket properties file");
+			return false;
+		}
+		return true;
+	}
+	
+	public BucketProperties readBucketProperties(File file) {
+		XStream xStream = new XStream();
+		FileReader reader;
+		try {
+			reader = new FileReader(file);
+		} catch (IOException e) {
+			log.error("Could not open bucket properties file " + file.getAbsolutePath(), e);
+			return null;
+		}
+		BucketProperties bucketProps = (BucketProperties)xStream.fromXML(reader);
+		try {
+			reader.close();
+		} catch (IOException e) {
+			log.error("Could not close bucket properties file", e);
+			return null;
+		}
+		return bucketProps;
+	}
+	
+	public static File createBucketSettingsFile(File basePath) {
+		String filename = basePath.getAbsolutePath() + "/" + "bucketProps.xml";
+		File xmlPath = new File(filename);
+		return xmlPath;
+	}
+}
---
+++ src/test/java/com/basho/proserv/datamigrator/riak/RiakBucketPropertiesTests.java
@@ -0,0 +1,102 @@
+package com.basho.proserv.datamigrator.riak;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import com.basho.riak.client.bucket.BucketProperties;
+import com.basho.riak.client.builders.BucketPropertiesBuilder;
+import com.basho.riak.client.cap.Quora;
+import com.basho.riak.client.cap.Quorum;
+import com.basho.riak.client.query.functions.NamedErlangFunction;
+import com.basho.riak.client.query.functions.NamedFunction;
+
+public class RiakBucketPropertiesTests {
+
+	@Test
+	public void testWriteReadBucketProperties() throws Exception {
+		TemporaryFolder tempFolder = new TemporaryFolder();
+		File propsFile = tempFolder.newFile();
+		
+		BucketPropertiesBuilder builder = new BucketPropertiesBuilder();
+		builder.addPrecommitHook(new NamedErlangFunction("named", "function"));
+		builder.addPostcommitHook(new NamedErlangFunction("bob","harry"));
+		builder.allowSiblings(true);
+		builder.backend("dandb");
+		builder.basicQuorum(true);
+		builder.bigVClock(1234567890);
+		builder.chashKeyFunction(new NamedErlangFunction("sally", "quenton"));
+//		builder.dw(1);
+//		builder.dw(Quora.fromString("one"));
+		builder.dw(new Quorum(1));
+		builder.lastWriteWins(true);
+		builder.linkWalkFunction(new NamedErlangFunction("whos", "on first"));
+		builder.notFoundOK(true);
+		builder.nVal(2);
+		builder.oldVClock(1234567889);
+//		builder.pr(1);
+//		builder.pr(Quora.fromString("one"));
+		builder.pr(new Quorum(1));
+//		builder.pw(1);
+//		builder.pw(Quora.fromString("one"));
+		builder.pw(new Quorum(1));
+//		builder.r(1);
+//		builder.r(Quora.fromString("one"));
+		builder.r(new Quorum(1));
+//		builder.rw(1);
+//		builder.rw(Quora.fromString("one"));
+		builder.rw(new Quorum(1));
+		builder.search(true);
+		builder.smallVClock(123456789);
+//		builder.w(1);
+//		builder.w(Quora.fromString("one"));
+		builder.w(new Quorum(1));
+		builder.youngVClock(1234567);
+		
+		BucketProperties props =  builder.build();
+		
+		RiakBucketProperties bucketProps = new RiakBucketProperties(new Connection());
+		
+		boolean success = bucketProps.writeBucketProperties(props, propsFile);
+		
+		assertTrue(success);
+		
+		BucketProperties readProps = bucketProps.readBucketProperties(propsFile);
+		
+		assertTrue(readProps != null);
+		NamedErlangFunction precommitFn = (NamedErlangFunction)readProps.getPrecommitHooks().toArray()[0];
+		assertTrue(precommitFn.getMod().compareTo("named") == 0);
+		assertTrue(precommitFn.getFun().compareTo("function") == 0);
+		NamedErlangFunction postcommitFn = (NamedErlangFunction)readProps.getPostcommitHooks().toArray()[0];
+		assertTrue(postcommitFn.getMod().compareTo("bob") == 0);
+		assertTrue(postcommitFn.getFun().compareTo("harry") == 0);
+		assertTrue(readProps.getAllowSiblings());
+		assertTrue(readProps.getBackend().compareTo("dandb") == 0);
+		assertTrue(readProps.getBasicQuorum());
+		assertTrue(readProps.getBigVClock() == 1234567890);
+		NamedErlangFunction chashFn = readProps.getChashKeyFunction();
+		assertTrue(chashFn.getMod().compareTo("sally") == 0);
+		assertTrue(chashFn.getFun().compareTo("quenton") == 0);
+		assertTrue(readProps.getDW().getIntValue() == 1);
+		assertTrue(readProps.getLastWriteWins());
+		NamedErlangFunction linkWalkFn = readProps.getLinkWalkFunction();
+		assertTrue(linkWalkFn.getMod().compareTo("whos") == 0);
+		assertTrue(linkWalkFn.getFun().compareTo("on first") == 0);
+		assertTrue(readProps.getNotFoundOK());
+		assertTrue(readProps.getNVal() == 2);
+		assertTrue(readProps.getOldVClock() == 1234567889);
+		assertTrue(readProps.getPR().getIntValue() == 1);
+		assertTrue(readProps.getPW().getIntValue() == 1);
+		assertTrue(readProps.getR().getIntValue() == 1);
+		assertTrue(readProps.getRW().getIntValue() == 1);
+		assertTrue(readProps.getSearch());
+		assertTrue(readProps.getSmallVClock() == 123456789);
+		assertTrue(readProps.getW().getIntValue() == 1);
+		assertTrue(readProps.getYoungVClock() == 1234567);
+	}
+
+}
