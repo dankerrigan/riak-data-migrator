d1c230e18ea9f3381fc406bbe327ec2582d81a4e
Dan Kerrigan <dankerrigan@basho.com>
2013-02-04 09:50:06 -0500

    Framework for resume function

---
+++ src/main/java/com/basho/proserv/datamigrator/io/UnwrittenKeyFinder.java
@@ -0,0 +1,91 @@
+package com.basho.proserv.datamigrator.io;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+public class UnwrittenKeyFinder implements Iterable<Key> {
+	private Set<Key> referenceKeySet = new HashSet<Key>();
+	private final Iterable<Key> referenceKeys;
+	private final Iterable<Key> compareKeys;
+	
+	public UnwrittenKeyFinder(Iterable<Key> referenceKeys, Iterable<Key> compareKeys) {
+		this.referenceKeys = referenceKeys;
+		this.compareKeys = compareKeys;
+		
+		this.loadKeys(this.referenceKeys, this.referenceKeySet);
+	}
+	
+	
+	public void loadReferenceKeys(Iterable<Key> keys) {
+		loadKeys(keys, referenceKeySet);
+	}
+		
+	public boolean keyExists(Key key) {
+		if (referenceKeySet.size() == 0) {
+			throw new IllegalArgumentException("Reference Keys have not been loaded");
+		}
+		
+		return this.referenceKeySet.contains(key);
+	}
+
+	@Override
+	public Iterator<Key> iterator() {
+		return new UnwrittenKeyIterator(this);
+	}
+	
+	private void loadKeys(Iterable<Key> keys, Set<Key> toLoad) {
+		toLoad.clear();
+		for (Key key : keys) {
+			toLoad.add(key);
+		}
+	}
+	
+	private class UnwrittenKeyIterator implements Iterator<Key> {
+		private final UnwrittenKeyFinder comparator;
+		private final Iterator<Key> keyIterator;
+		
+		private Key nextKey = null;
+		
+		public UnwrittenKeyIterator(UnwrittenKeyFinder comparator) {
+			this.comparator = comparator;
+			this.keyIterator = comparator.compareKeys.iterator();
+			this.setNextKey();
+		}
+		
+		@Override
+		public boolean hasNext() {
+			return nextKey == null;
+		}
+
+		@Override
+		public Key next() {
+			Key currentKey = this.nextKey;
+			this.setNextKey();
+			
+			return currentKey;
+		}
+		
+		private void setNextKey() {
+			try {
+				while (!Thread.interrupted()) {
+					Key key = keyIterator.next();
+					if (!this.comparator.keyExists(key)) {
+						nextKey = key;
+						break;
+					}
+				}
+			} catch (NoSuchElementException e) {
+				nextKey = null;
+			}
+		}
+
+		@Override
+		public void remove() {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+	
+}
---
+++ src/test/java/com/basho/proserv/riak/datamigrator/io/KeyJournalComparatorTests.java
@@ -0,0 +1,55 @@
+package com.basho.proserv.riak.datamigrator.io;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import com.basho.proserv.datamigrator.io.Key;
+import com.basho.proserv.datamigrator.io.KeyJournal;
+import com.basho.proserv.datamigrator.io.UnwrittenKeyFinder;
+
+public class KeyJournalComparatorTests {
+
+	@Test
+	public void test() throws Exception {
+		String[] buckets = {"A", "B", "C", "D", "E", "F"};
+		int KEY_COUNT = 100000;
+		
+		TemporaryFolder tempFolder = new TemporaryFolder();
+		
+		File keyFile = tempFolder.newFile();
+		File keyFile2 = tempFolder.newFile();
+		
+		KeyJournal keyJournal = new KeyJournal(keyFile, KeyJournal.Mode.WRITE);
+		KeyJournal keyJournal2 = new KeyJournal(keyFile2, KeyJournal.Mode.WRITE);
+		
+		for (String bucket : buckets) {
+			for (Integer i = 0; i < KEY_COUNT; ++i) {
+				keyJournal.write(bucket, i.toString());
+				if (i % 2 == 0) {
+					keyJournal2.write(bucket, i.toString());
+				}
+			}
+		}
+		
+		keyJournal.close();
+		keyJournal2.close();
+		
+		KeyJournal readKeyJournal = new KeyJournal(keyFile, KeyJournal.Mode.READ);
+		KeyJournal readKeyJournal2 = new KeyJournal(keyFile2, KeyJournal.Mode.READ);
+		
+		UnwrittenKeyFinder comparator = new UnwrittenKeyFinder(readKeyJournal, readKeyJournal2);
+		
+		for (Key key : comparator) {
+			assertTrue(Integer.parseInt(key.key()) % 2 != 0);
+		}
+		
+		readKeyJournal.close();
+		readKeyJournal2.close();
+	}
+
+}
